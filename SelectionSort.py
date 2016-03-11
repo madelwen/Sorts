9099c2069dfdd431f3b02e5be34572ce8de402e0
Maddie Wen <madelinemwen@gmail.com>
2016-03-10 23:28:17 -0600

    Update SelectionSort.py

--- SelectionSort.py
+++ SelectionSort.py
@@ -5,19 +5,15 @@ nums = [int(n) for n in numz]
 end = len(nums) - 1
 
 print "Start: ", nums
-i = 0
-while i != end:
+
+for i in range(len(nums)):
 	baby = nums[i]
 	minindex = i
-	j = i + 1
-	while j < len(nums):
+	for j in range(i+1,len(nums)):
 		if nums[j] < nums[minindex]:
 			minindex = j
-		j = j + 1
 	if i != minindex:
 		temp = nums[i]
 		nums[i] = nums[minindex]
 		nums[minindex] = temp
 	print nums
-		
-	i = i + 1
