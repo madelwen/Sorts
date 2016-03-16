3e0b04e1d180d62367d04d93dd7f7b819f103057
Maddie Wen <madelinemwen@gmail.com>
2016-03-15 23:12:03 -0500

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -1,31 +1,35 @@
 import math
 from selectionsort import randNum
 
-def part(nums, l, r):
-	i = l
-	j = r
-	pivot = nums[(l+r)/2]
-	while i <= j:
-		while nums[i] < pivot:
-			i = i + 1
-		while nums[j] > pivot:
-			j = j - 1
-		if i <= j:
-			tmp = nums[i]
-			nums[i] = nums[j]
-			nums[j] = tmp
-			i = i + 1
-			j = j - 1
-	return i
-	
-def qwik(nums, l, r):
-	index = part(nums, l, r)
-	if (l < index - 1):
-		qwik(nums, l, index - 1)
-	if (index < r):
-		qwik(nums, index, r)
-	return nums
+def qwik(nums):
+	l = 0
+	r = len(nums) - 1
+	def part(nums, l, r):
+		i = l
+		j = r
+		pivot = nums[(l+r)/2]
+		while i <= j:
+			while nums[i] < pivot:
+				i = i + 1
+			while nums[j] > pivot:
+				j = j - 1
+			if i <= j:
+				tmp = nums[i]
+				nums[i] = nums[j]
+				nums[j] = tmp
+				i = i + 1
+				j = j - 1
+		return i
+		
+	def qwikSort(nums, l, r):
+		index = part(nums, l, r)
+		if (l < index - 1):
+			qwikSort(nums, l, index - 1)
+		if (index < r):
+			qwikSort(nums, index, r)
+		return nums
+	return qwikSort(nums, l, r)
 		
 nums = randNum(10, 0, 100)
 print nums
-print qwik(nums, 0, len(nums)-1)
+print qwik(nums)
